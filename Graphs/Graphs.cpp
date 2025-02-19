4bfae98e5a3e9a8942bb3cf89118c4b403ab998f
seolaaaa <kennethandreihipos@gmail.com>
2025-02-19 18:41:39 +0800

    Graphs.cpp

---
+++ Graphs/Graphs.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class Graph {
+private:
+    int V;
+    vector<vector<int>> adjList;
+
+public:
+    Graph(int vertices) {
+        V = vertices;
+        adjList.resize(V);
+    }
+
+    void addEdge(int u, int v) {
+        adjList[u].push_back(v);
+        adjList[v].push_back(u);
+    }
+
+    void display() {
+        for (int i = 0; i < V; i++) {
+            cout << "Vertex " << i << " -> ";
+            for (int neighbor : adjList[i]) {
+                cout << neighbor << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    void BFS(int start) {
+        vector<bool> visited(V, false);
+        queue<int> q;
+        visited[start] = true;
+        q.push(start);
+
+        cout << "BFS Traversal: ";
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            cout << node << " ";
+
+            for (int neighbor : adjList[node]) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    q.push(neighbor);
+                }
+            }
+        }
+        cout << endl;
+    }
+
+    void DFSUtil(int node, vector<bool>& visited) {
+        visited[node] = true;
+        cout << node << " ";
+
+        for (int neighbor : adjList[node]) {
+            if (!visited[neighbor])
+                DFSUtil(neighbor, visited);
+        }
+    }
+
+    void DFS(int start) {
+        vector<bool> visited(V, false);
+        cout << "DFS Traversal: ";
+        DFSUtil(start, visited);
+        cout << endl;
+    }
+};
+
+int main() {
+    Graph g(5);
+    g.addEdge(0, 1);
+    g.addEdge(0, 4);
+    g.addEdge(1, 2);
+    g.addEdge(1, 3);
+    g.addEdge(1, 4);
+    g.addEdge(2, 3);
+    g.addEdge(3, 4);
+
+    cout << "Graph Representation:\n";
+    g.display();
+
+    g.BFS(0);
+    g.DFS(0);
+
+    return 0;
+}
